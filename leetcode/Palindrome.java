a3ce8a6e5d3d1743dd34e72e59ecabefb3968207
jmac <go.donghyeon@jtbc.co.kr>
2020-03-25 22:07:48 +0900

    leetcode Palindrome 추가 설명

--- leetcode/Palindrome.java
+++ leetcode/Palindrome.java
@@ -8,6 +8,11 @@ public class Palindrome {
         System.out.println(palindrome.isPalindrome( "A man, a plan, a canal: Panama" ));
     }
 
+    /**
+     *  펠린드롬 : 문장을 뒤집어도 동일한 형태, 숫자, 알파벳 처리, 사이즈 0도 트루, 기타 특수문자 모두 무시 트루.
+     * @param string
+     * @return boolean
+     */
 
     public boolean isPalindrome(String s) {
         int endPoint = s.length()-1;
