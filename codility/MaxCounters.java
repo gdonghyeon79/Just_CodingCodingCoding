914e1d26abae9d279fa105327bb01dcff9d51a44
gdonghyeon <gdongheyon79@gamil.com>
2020-03-29 21:32:36 +0900

    [코딜리티] MaxCounters : 1차 정확함 100%, 속도 77% = 점수 77%. 2차에서 퍼모먼스 개선 필요. 2차 개선 버전 100%, 100% = 점수 100%. 퍼포먼스.역시 for문을 최소화 시킴.

--- codility/MaxCounters.java
+++ codility/MaxCounters.java
@@ -16,27 +16,37 @@ public class MaxCounters {
         System.out.println("result -- > " + solution(N, A));
     }
 
-    // 첫 시도, 77%  정확함 100%, 퍼포먼스 77%..  획득.
+    // 첫 시도 : 77%  정확함 100%, 퍼포먼스 77%..  획득.
+    // 2ㅊㅏ 시도 : max 값을 찾기 위한 for문 제거 목적. for 문에 for은 성능에 치명적.
+    // for문을 따로 두번, N+1 값을 기준으로 이하 값은 없음, max 값에 인크리즈, 나중에 max값보다 낮은 위치 값만 max 값으로 대체 후 리턴.
+
     public static int[] solution(int N, int[] A) {
         // write your code in Java SE 8
-        int alen = A.length;
+        int aLen = A.length;
         int[] B = new int[N];
-        int maxValue = 0;
-        for(int i=0; i< alen; i++){
-            int aValue = A[i];
 
-            if(aValue == (N+1)){
-                for(int j=0; j<N ; j++){
-                    B[j] = maxValue;
-                }
+        int maxVal = 0;
+        int maxTmp = 0;
+
+        for(int i=0; i<aLen; i++){
+            int aVal = A[i];
+            if(aVal == (N+1)){
+                maxVal = maxTmp;
             }else{
-                int bPoint = aValue-1;
-                int beforeValue = B[bPoint];
-                B[bPoint] = beforeValue + 1;
-
-                if(maxValue < B[bPoint]){
-                    maxValue = B[bPoint];
+                int beforeVal = B[aVal-1];
+                if(beforeVal <= maxVal){
+                    B[aVal-1] = maxVal+1;
+                }else{
+                    B[aVal-1] = beforeVal+1;
                 }
+                if(B[aVal-1] > maxTmp){
+                    maxTmp = B[aVal-1];
+                }
+            }
+        }
+        for(int j=0; j<B.length;j++){
+            if(B[j]<maxVal){
+                B[j] = maxVal;
             }
         }
         return B;
