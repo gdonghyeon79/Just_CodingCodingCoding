3533d7c177153abd4103cec596ce8c3cf7386c20
jmac <go.donghyeon@jtbc.co.kr>
2020-04-08 12:59:52 +0900

    leetcode CountAndSay - 패턴을 읽어야하고, 재귀를 생각했어야 한다. 그리고 풀어내는 방법도 간결하게 유추할 필요 있음.

---
+++ leetcode/CountAndSay.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class CountAndSay {
+    public static void main(String[] args) {
+        int n =1;
+        if(n<=0){
+            return "-1";
+        }
+
+        String result = "1";
+        for(int i=1;i<n;i++){
+            result = method(result);
+        }
+
+        System.out.println(result);
+     }
+
+    public static String method(String result){
+        StringBuilder sb = new StringBuilder();
+        int p = 0;
+        while(p<result.length()){
+            char ch = result.charAt(p);
+            int count = 0;
+
+            while(p<result.length() && ch == result.charAt(p)){
+                p++;
+                count++;
+            }
+            sb.append(count);
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}
